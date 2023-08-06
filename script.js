7b7d5a3b70ac3a9706b9629aae6020be11f4d932
ReeperC3 <cian3neiland@gmail.com>
2023-08-06 20:44:21 +0100

    Created the calculator, made buttons and functions for the operations.

---
+++ script.js
@@ -0,0 +1,25 @@
+const numbers = document.querySelector('.num')
+const operators = document.querySelector('.operator')
+let currentOperator = '';
+let firstNum = '';
+let secondNum = '';
+
+function add(num1, num2) {
+  num = num1 + num2;
+  return num;
+}
+
+function subtract(num1, num2) {
+  num = num1 + num2;
+  return num;
+}
+
+function multiply(num1, num2) {
+  num = num1 * num2;
+  return num;
+}
+
+function divide(num1, num2) {
+  num = num1 / num2;
+  return num;
+}
